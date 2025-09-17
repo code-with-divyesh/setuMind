c6dc7b5a3c447f9e1f5e5fdcda8c3f2e97380bc4
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-09-18 01:29:07 +0530

    remove imort

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,11 +1,10 @@
 // src/assets/assets.js
 
 import logo from "./logo.png";
-import alram from "./alram.mp3";
+
 // agar future me aur images ho to yaha add karte jao
 // import bg from "./setuMindBg.avif";   <-- example
 
 export default {
   logo,
-  alram,
 };
